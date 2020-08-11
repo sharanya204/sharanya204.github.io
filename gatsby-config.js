ff65d780dfb7acef9e20bc41097b767ac260368a
sharanya204 <41498182+sharanya204@users.noreply.github.com>
2020-08-11 01:06:03 -0700

    updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Sharanya`,
+        short_name: `Sharanya`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
